041936838d4f30aa6e28c0c019997dc0efeac663
cadensdavis <c.d.stewart333@gmail.com>
2024-01-10 12:48:04 -0700

    messing around in the Sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine(" ");
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine(" ");
+        Console.WriteLine("I hope this time around I can complete this class");
+        Console.WriteLine(" ");
     }
 }
