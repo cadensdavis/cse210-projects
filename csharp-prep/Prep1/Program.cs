e27db35e9137c5c0477ec9c3711999a7e12a12e1
cadensdavis <c.d.stewart333@gmail.com>
2024-01-13 20:17:07 -0700

    Completed the Prep 1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Tidyness
+        Console.WriteLine("");
+
+        Console.Write ("What is your FIRST name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write ("What is your LAST name? ");
+        string lastName = Console.ReadLine();
+        
+        //Tidyness
+        Console.WriteLine("");
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+
+        //tidyness
+        Console.WriteLine("");
+        
+        // //Tidyness
+        // Console.WriteLine("");
+
+        // //WriteLine Practice
+        // Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("This is written in C#");
+
+        // //Tidyness
+        // Console.WriteLine("");
+
+        // //Question
+        // Console.Write("What is your favorite color? ");
+        // string FavColor =Console.ReadLine();
+        // Console.WriteLine($"Your favorite color is {FavColor}");
+
+        // //Tidyness
+        // Console.WriteLine("");
+        
     }
 }
