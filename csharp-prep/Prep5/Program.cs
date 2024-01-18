7c7b5bac5249b75b26def458945c78c8387a7439
cadensdavis <c.d.stewart333@gmail.com>
2024-01-18 00:11:21 -0700

    Prep 5 Completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,61 @@
 using System;
 
-class Program
+namespace ListManipulation
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void Main(string[] args)
+        {
+            //Spacing
+            Console.WriteLine("");
+
+            //Running all functions
+            DisplayWelcome();
+
+            string username = PromptUserName();
+
+            int usernumber = PromptUserNumber();
+
+            int squarednumber = SquareNumber(usernumber);
+
+            DisplayResults(username, squarednumber);
+
+
+            //Print Welcome
+            void DisplayWelcome()
+            {
+                Console.WriteLine("Welcome to the Program!");
+            }
+
+            //Asking for name and returning
+            static string PromptUserName()
+            {
+                Console.Write("What is your name? ");
+                string username = Console.ReadLine();
+                return username;
+            }
+
+            //Asking for number and returning
+            static int PromptUserNumber()
+            {
+                Console.Write("What is your favorite number? ");
+                string number = Console.ReadLine();
+                int usernumber = int.Parse(number);
+                return usernumber;
+            }
+
+            //Taking number and squaring it
+            static int SquareNumber(int number)
+            {
+                int squarednumber = number * number;
+                return squarednumber;
+            }
+
+            //Pulling it all together and taking username and usernumber as parameters
+            void DisplayResults(string username ,int usernumber)
+            {
+                Console.WriteLine($"{username}, the square of your number is {usernumber}");
+            }
+        }
     }
 }
