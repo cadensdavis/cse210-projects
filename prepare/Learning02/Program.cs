4d27bb17778347d617dd5f693b4f3ad9169eb274
cadensdavis <c.d.stewart333@gmail.com>
2024-01-23 11:53:45 -0700

    Wednesday Abstraction Homework

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Del Taco";
+        job1._jobTitle = "Fry Cooker";
+        job1._startYear = 1706;
+        job1._endYear = 1000;
+
+        //job1.Display();
+
+        Job job2 = new Job();
+        job2._company = "Taco Bell";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2000;
+        job2._endYear = 2020;
+
+        //job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Caden S. Davis";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
