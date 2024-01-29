50ca74f48bc78cef36962d7b1d140a806ae68f84
cadensdavis <c.d.stewart333@gmail.com>
2024-01-29 13:58:11 -0700

    Adding Methods, and attributres

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,4 +1,42 @@
 public class Entry
 {
+    public string prompt;
+    public string date;
+    public string entry;
+
+
+    private void Display()
+    {
+
+    }
+
+    private void GetResponse()
+    {
+        
+    }
+
+    private void GetDate()
+    {
+        
+    }
+
+    private void SetPrompt()
+    {
+        
+    }
+
+    private void SetResponse()
+    {
+        
+    }
+
+    private void SetDate()
+    {
+        
+    }
+    
+    
+    
+    
     
 }
